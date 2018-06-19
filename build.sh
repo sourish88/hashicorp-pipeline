5855d6796bae5b7aecc801488be394a1aec34662
Simon McCartney <simon.mccartney@taskize.com>
2018-06-19 23:46:20 +0100

    Detect GitHub org and project name

--- build.sh
+++ build.sh
@@ -6,9 +6,18 @@ if [[ -L "${THIS_SCRIPT}" ]]; then
 fi
 SCRIPT_HOME="$( cd "$( dirname "${THIS_SCRIPT}" )/" && pwd )"
 
-GITHUB_URL=""
-GITHUB_ORG=""
-GITHUB_PROJECT=""
+# git@github.com:jenkins201/terraform-iac.git
+# https://github.com/jenkins201/terraform-iac.git
+GITHUB_URL=$(git remote get-url origin)
+#GITHUB_URL="https://github.com/jenkins201/terraform-iac.git"
+GITHUB_REGEX="(git@github.com:|https://github.com/)([a-zA-Z0-9]+)/([a-zA-Z0-9\-]+).git"
+if [[ $GITHUB_URL =~ $GITHUB_REGEX ]]; then
+  export GITHUB_ORG="${BASH_REMATCH[2]}"
+  export GITHUB_PROJECT="${BASH_REMATCH[3]}"
+else
+  echo "ERROR: Can't detect GITHUB_ORG and GITHUB_PROJECT"
+  exit 2
+fi
 
-docker build -f Dockerfile -t jenkins201/hashicorp-ci .
-docker push jenkins201/hashicorp-ci:latest
+docker build -f Dockerfile -t ${GITHUB_ORG}/${GITHUB_PROJECT} .
+docker push ${GITHUB_ORG}/${GITHUB_PROJECT}:latest
